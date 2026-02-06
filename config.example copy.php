316d16a3112e5e0c98b75f9bf64a0b3e8fa82eb6
John <johnbloome@gmail.com>
2026-02-06 07:41:27 -0600

    Delete config.example copy.php

--- config.example copy.php
+++
@@ -1,5 +0,0 @@
-<?php
-$DB_HOST = 'localhost'; 
-$DB_USER = 'db_user'; //change to your database user
-$DB_PASS = 'db_password'; //change to your password
-$DB_NAME = 'db_name'; //change to your database name
